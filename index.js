4b0c04132bfd9a0b91b2085dff4577682e780c23
CalebSamson <62663047+CalebSamson@users.noreply.github.com>
2020-03-29 20:36:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+
+
+
+var clicks = 0;
+
+function clickCounter() {
+	clicks++;
+	var numClicksSpan = document.getElementById('numClicks');
+	if (clicks == 1)
+		numClicksSpan.innerHTML = 'once';
+	else
+		numClicksSpan.innerHTML = clicks + ' times';
+}
+
+var button = document.getElementById('clickHere');
+button.addEventListener('click', clickCounter);
+
+//hover over
+
+
+function makeBold() {
+	var divs = document.getElementsByClassName('highlightText');
+	for (var i = 0; i < divs.length; i++) {
+		divs[i].style.color = 'green';
+		divs[i].style.fontWeight = 'bold';
+	}
+}
+
+function restore() {
+	var divs = document.getElementsByClassName('highlightText');
+	for (var i = 0; i<divs.length; i++) {
+		divs[i].style.color = 'black';
+		divs[i].style.fontWeight = 'normal';
+	}
+}
+
+var mouseOverMeDiv = document.getElementById('mouseOverMe');
+mouseOverMeDiv.addEventListener('mouseover', makeBold);
+mouseOverMeDiv.addEventListener('mouseout', restore);
+
+//input into box
+
+
+function nameHandler(e) {
+	if (e.keyCode == 13) {
+		var nameInput = document.getElementById('nameInput');
+		var nameField = document.getElementById('nameField');
+		nameField.innerHTML = nameInput.value;
+		nameField.style.backgroundColor = 'pink';
+		nameField.style.textTransform = 'capitalize';
+
+	}
+}
+
+document.addEventListener('keyup', nameHandler);
+
+//Magic 8 ball question asker
+
+function questionDisplayer(q) {
+	if (q.keyCode == 13) {
+		var questionInput = document.getElementById('questionInput');
+		var questionField = document.getElementById('questionField');
+		questionField.innerHTML = questionInput.value;
+		questionField.style.backgroundColor = 'yellow'
+		
+	}
+}
+
+document.addEventListener('keyup', questionDisplayer);
+
+//magic 8 ball continue button
+
+function eightBall() {
+
+	let yourFate = '';
+	randomNumber = Math.floor(Math.random() * 8)
+	let eightBall = ''
+
+		if (randomNumber == 0) {yourFate = 'Absolutely!'}
+		if (randomNumber == 1) {yourFate = 'Sure, why not?'}
+		if (randomNumber == 2) {yourFate = 'Heck Yeah!'}
+		if (randomNumber == 3) {yourFate = 'I feel like that is more of a presonal problem...'}
+		if (randomNumber == 4) {yourFate = 'Absolutely not, but good try!'}
+		if (randomNumber == 5) {yourFate = 'Nope!'}
+		if (randomNumber == 6) {yourFate = 'I would not count it out!'}
+		if (randomNumber == 7) {yourFate = 'Do you want me to be honest?'}
+
+
+
+  document.getElementById("answer").innerHTML = yourFate;
+}
+
